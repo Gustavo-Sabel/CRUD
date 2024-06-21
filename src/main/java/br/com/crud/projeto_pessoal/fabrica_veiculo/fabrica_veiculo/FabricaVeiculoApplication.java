09625022786737e0feccc505bcaee8bc451c17e8
GUSTAVO DE SOUZA SABEL <gustavo_sabel@estudante.sesisenai.org.br>
2024-06-20 21:02:42 -0300

    Configurando o Application Properties

---
+++ src/main/java/br/com/crud/projeto_pessoal/fabrica_veiculo/fabrica_veiculo/FabricaVeiculoApplication.java
@@ -0,0 +1,13 @@
+package br.com.crud.projeto_pessoal.fabrica_veiculo.fabrica_veiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaVeiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaVeiculoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/crud/projeto_pessoal/fabrica_veiculo/fabrica_veiculo/FabricaVeiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crud.projeto_pessoal.fabrica_veiculo.fabrica_veiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricaVeiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
