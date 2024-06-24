fd0662ffc6ecf894ecb40a7c3718631f20901182
GUSTAVO DE SOUZA SABEL <gustavo_sabel@estudante.sesisenai.org.br>
2024-06-24 18:55:40 -0300

    Criação do Spring Boot

---
+++ fabrica-de-veiculo/src/main/java/br/com/crud/prjeto/fabrica_veiculo/FabricaDeVeiculoApplication.java
@@ -0,0 +1,13 @@
+package br.com.crud.prjeto.fabrica_veiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaDeVeiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaDeVeiculoApplication.class, args);
+	}
+
+}
---
+++ fabrica-de-veiculo/src/test/java/br/com/crud/prjeto/fabrica_veiculo/FabricaDeVeiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crud.prjeto.fabrica_veiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricaDeVeiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
